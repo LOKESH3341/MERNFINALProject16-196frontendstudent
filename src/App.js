90092f8f58edb01efb5f686c7fced42f03fa23bb
Your Name <you@example.com>
2023-11-20 14:13:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter, Routes, Route } from 'react-router-dom';
+// import CreateWork from './components/CreateWork';
+// import WorkList from './components/WorkList';
+import Nav from './components/Nav';
+// import EditWork from './components/EditWork';
+// import StudentView from './components/StudentView';
+import MainPage from './components/MainPage';
+import WorkList from './components/WorkList' 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <HashRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          
+          <Route path="/work-list" element={<WorkList />} />
+          
+          
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
 
 export default App;
+
